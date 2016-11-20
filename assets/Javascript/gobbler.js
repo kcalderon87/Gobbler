657888bf7278d919595cdb39514311d07e06afba
Guillermo <Guillermo@Guillermos-MacBook-Pro.local>
2016-11-20 13:56:08 -0500

    completed random imageLoop & added script

--- assets/Javascript/gobbler.js
+++ assets/Javascript/gobbler.js
@@ -1,8 +1,9 @@
 $('document').ready(function(){
-//console.log("start menu")
+
+
 
 // holds background images
-var imgArray=['../Gobbler/assets/images/foodImage1.jpg', 
+var images=['../Gobbler/assets/images/foodImage1.jpg', 
 			  '../Gobbler/assets/images/foodImage2.jpg', 
 			  '../Gobbler/assets/images/foodImage3.jpg', 
 			  '../Gobbler/assets/images/foodImage4.jpg', 
@@ -10,24 +11,76 @@ var imgArray=['../Gobbler/assets/images/foodImage1.jpg',
 			  '../Gobbler/assets/images/foodImage6.jpg', 
 			  '../Gobbler/assets/images/foodImage7.jpg', 
 			  '../Gobbler/assets/images/foodImage8.jpg', 
-			  '../Gobbler/assets/images/foodImage10.jpg'
+			  '../Gobbler/assets/images/foodImage9.jpg',
+			  '../Gobbler/assets/images/foodImage10.jpg',
+			  '../Gobbler/assets/images/foodImage11.jpg',
+			  '../Gobbler/assets/images/foodImage12.jpg', 
+			  '../Gobbler/assets/images/foodImage13.jpg', 
+			  '../Gobbler/assets/images/foodImage14.jpg', 
+			  '../Gobbler/assets/images/foodImage15.jpg', 
+			  '../Gobbler/assets/images/foodImage16.jpg', 
+			  '../Gobbler/assets/images/foodImage17.jpg', 
+			  '../Gobbler/assets/images/foodImage18.jpg',
+			  '../Gobbler/assets/images/foodImage19.jpg', 
+			  '../Gobbler/assets/images/foodImage20.jpg'
 			  ];
 
-console.log("yummy food: " + imgArray[4])
+//--------------HOME PAGE RANDOM BACKGROUND LOOP STARTS-------------------
+console.log("-----Backgound ImageLoop at Random-----")
 
-//random number generator for images in background
-var i = Math.floor(imgArray.length * Math.random())
+var remIndex;
 
-//holds the value assigned to background image
-var imageBackground = 'url(' + imgArray[i] + ')';
+// The index variable will keep track of which image is currently showing
+var index = 0,oldIndex;
 
-console.log(imageBackground)
+$(document).ready(function() {
+    
+    // Call backstretch for the first time,
+    // set speed of for a fadeIn effect between images.
+    getNextImage();   
+    $.backstretch(images[index], {
+        speed: 3000
+    });
 
-//when the page loads perform css functions
-$('body').css({
-	"background-image": imageBackground,
-	"background-repeat": "no-repeat",
-	"background-size": "100%"
-})
+    // Set an interval that increments the index and sets the new image
+    setInterval(function() {
+        getNextImage();           
+        $.backstretch(images[index]);
+    }, 6000);
+
+    // script for preloading all of the images
+    $(images).each(function() {
+        $("<img/>")[0].src = this;
+    });   
+});
+
+function getNextImage() {
+    if(remIndex == null || remIndex.length == 0) {
+       repopulate(); 
+    }
+    oldIndex = index;
+    
+    var rInd;
+    while (oldIndex == index) {
+        rInd = Math.floor((Math.random()*remIndex.length));
+        index = remIndex[rInd];
+    }
+    console.log(index);
+    remIndex.splice(rInd,1);    
+}
+
+function repopulate() {
+    if(remIndex == null) {
+       remIndex = new Array(); 
+    }
+    for(var i = 0; i<images.length; i++) {
+        remIndex.push(i);
+    }
+}
+//---------------------END OF HOMEPAGE RANDOM BACKGROUND LOOP------------
+
+
+
+
 
 }) // document ready 
